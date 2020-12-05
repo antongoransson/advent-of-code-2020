30cce4d35f2c0089abc7a57d21958ca3f3cd24b2
antongoransson <anton.goransson95@gmail.com>
2020-12-05 11:07:26 +0100

    Update to cleaner solution for day05

--- day05/day05.py
+++ day05/day05.py
@@ -1,35 +1,24 @@
 import math
 
 def get_seat_n(seat_n):
-    r_a = 0
-    r_b = 127
-    c_a = 0
-    c_b = 7
-    for c in seat_n:
-        if c == 'F':
-            r_b = r_b - math.ceil(((r_b - r_a) / 2))
-        elif c == 'B':
-            r_a = r_a + math.ceil((r_b - r_a) / 2)
-        elif c == 'L':
-            c_b = c_b - math.ceil(((c_b - c_a) / 2))
-        elif c == 'R':
-            c_a = c_a + math.ceil(((c_b - c_a) / 2))
-    return r_a * 8 + c_a
+    row_n = seat_n[:7].replace('F', '0').replace('B', '1')
+    col_n = seat_n[-3:].replace('L', '0').replace('R','1')
+    return int(row_n, 2) * 8  + int(col_n, 2)
 
 def solve_part_1(seats):
     return max([get_seat_n(seat) for seat in seats])
 
 def solve_part_2(seats):
-    seat_numbers = set([get_seat_n(seat) for seat in seats])
-    for row in range(2, 128):
-        for col in range(0, 8):
-            if row * 8 + col not in seat_numbers:
-                return row * 8 + col
+    seat_ids = set([get_seat_n(seat) for seat in seats])
+    for i in seat_ids:
+        if i + 1 not in seat_ids and i + 2 in seat_ids:
+            return i + 1
 
 
 def main():
     with open('input.txt') as f:
-        seats = [line for line in f.readlines()]
+        seats = [line.strip() for line in f.readlines()]
+        print(seats)
     sol1 = solve_part_1(seats)
     print('Part 1: {}'.format(sol1))
     
